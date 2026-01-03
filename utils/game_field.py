357e8f764ba94a0c4bd4dfc77e5dca9e904fff17
ching <ching7598@gmail.com>
2026-01-03 11:27:21 +0800

    fix outSIde bug

--- utils/game_field.py
+++ utils/game_field.py
@@ -46,10 +46,30 @@ class FieldManager:
         self.playerList=list();
 
     def ball_outside(self,ball):
+        
+        length_vector=self.ground.axis
+        width_vector=length_vector.cross(self.ground.showObj.up)
+        
         delta_pos=ball.pos_center-self.ground.pos_center
-        width_vector=self.ground.axis
-        length_vector=width_vector.cross(self.ground.showObj.up)
-        return abs(dot(delta_pos,width_vector))>(self.ground.field_length/2) or abs(dot(delta_pos,length_vector))>(self.ground.field_width/2)
+        pos_as_width=dot(delta_pos,width_vector)
+        pos_as_length=dot(delta_pos,length_vector)
+        outSide=False
+        while abs(pos_as_width)>(self.ground.field_width/2):
+            outSide=True
+            sign=(pos_as_width>0)-(pos_as_width<0)
+            ball.set_posCenter(ball.pos_center-width_vector*sign)
+            delta_pos=ball.pos_center-self.ground.pos_center
+            pos_as_width=dot(delta_pos,width_vector)
+        while abs(pos_as_length)>(self.ground.field_length/2):
+            outSide=True
+            sign=(pos_as_length>0)-(pos_as_length<0)
+            ball.set_posCenter(ball.pos_center-length_vector*sign)
+            delta_pos=ball.pos_center-self.ground.pos_center
+            pos_as_length=dot(delta_pos,length_vector)
+        
+        if outSide:
+            Physis.position_when_onGround(ball)
+        return outSide
         
         
     def push_into_field(self,physbj=None,posX=None,posZ=None):
@@ -78,11 +98,10 @@ class FieldManager:
             #看是否進球
             if (self.leftGoal.ball_touch_goal(ball)) or (self.rightGoal.ball_touch_goal(ball)):
                 Physis.position_when_onGround(ball,0,0)
+                ball.velocity=vector(0,0,0)            
+            elif self.ball_outside(ball): 
                 ball.velocity=vector(0,0,0)
-            #看是否出界
-            elif ball.velocity.mag>0.1 and self.ball_outside(ball):
-                Physis.position_when_onGround(ball)
-                ball.velocity=vector(0,0,0)
+                print("Ball outside!")               
             else:
                 ball.next_state()
         
