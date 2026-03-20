41eb1d5895f591b6918729d91c144f53a4ddf7eb
unknown <ching7598@gmail.com>
2026-03-20 21:46:35 +0800

    wiper added

---
+++ obj/physical_object/player_wiper.py
@@ -0,0 +1,47 @@
+from obj.physical_object.player import Player, MemoryObj
+from utils.physics_controller import Physis
+from vpython import *
+import math
+
+        
+
+class Player_wiper(Player):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+ 
+    def wipe_out(self,mem_ball,mem_target_goal):
+        if mag(mem_ball.pos_center-self.leg_range.pos)<(self.leg_range.radius+ball.radius):
+            dot_value=dot(self.axis,(mem_target_goal.pos_center-self.pos_center))
+            if dot_value<0:
+                wipe_vector=mem_target_goal.realObj.axis+vector(0,1,0)
+                self.kick_ball(mem_ball.realObj,kick_force=wipe_vector*2000)
+
+            return True
+        else:
+            return False
+    
+    def think(self):#只會一直找球並追著球跑，一旦追到球就轉身對準球門，然後全力射
+        self.memory_update() 
+        memBall=self.find_something("memBall","ball")#找球
+        
+        mem_target_goal=self.memoryDict["target_goal_0"]
+        if mem_target_goal is None:#如果還沒被指定到隊，沒有目標球門，就不動
+            return
+        
+        if memBall is not None:
+            if not self.shoot(memBall,mem_target_goal):                    
+                self.chasing_ball(memBall,0)
+                   
+
+            
+  
+                    
+        
+        
+        
+                    
+
+        
+    
+            
+
